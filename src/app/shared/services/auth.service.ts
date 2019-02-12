9892fd96014265590cb823c8a649d6460c766e33
stephanogiuseppe <stephano.grp@gmail.com>
2019-02-12 17:37:36 -0200

    add storage service, refactor auth service (use storage service)

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 import { BehaviorSubject } from 'rxjs';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,11 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private router: Router
-  ) { }
+    private router: Router,
+    private storageService: StorageService,
+  ) {
+    this.verifySessionExists();
+  }
 
   public signUp(email: string, password: string): boolean {
     return true;
@@ -23,8 +27,7 @@ export class AuthService {
 
   public signIn(email: string, password: string): boolean {
     if (email === 'test@test.com' && password === '123456') {
-      const user = { email, password };
-      localStorage.setItem('token', JSON.stringify(user));
+      this.storageService.setItemStorage('token', JSON.stringify({ email, password }));
       this.changeLogged(true);
       this.router.navigate(['/home']);
       return true;
@@ -35,6 +38,14 @@ export class AuthService {
   }
 
   public signOut(): void { }
+
+  private verifySessionExists() {
+    const userDataLocalStorage = this.storageService.getValueFromKeyStorage('token');
+    if (userDataLocalStorage) {
+      this.changeLogged(true);
+      this.router.navigate(['/home']);
+    }
+  }
 
   private changeLogged(value): void {
     this.authenticateUser.next(value);
