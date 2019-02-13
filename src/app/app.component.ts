f8fb38077552709ea2ba31a624c4764492b362ee
stephanogiuseppe <stephano.grp@gmail.com>
2019-02-12 22:07:35 -0200

    app component route controller (auth and home route verify)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { AuthService } from './shared/services/auth.service';
 
@@ -9,11 +10,19 @@ import { AuthService } from './shared/services/auth.service';
 })
 export class AppComponent implements OnInit {
 
+  public homeRoute: boolean;
   public userAuthenticated: boolean;
 
   constructor(
-    private authService: AuthService
-  ) { }
+    private authService: AuthService,
+    private router: Router
+  ) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationEnd) {
+        event.url === '/home' ? this.homeRoute = true : this.homeRoute = false;
+      }
+    });
+  }
 
   ngOnInit() {
     this.verifyUserAuthenticated();
